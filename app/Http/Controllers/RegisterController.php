66c0b010355b9aa7f243fb3f0730b7040ec66c26
Jonathan Hernandez <jonathan_fenix_95@hotmail.com>
2022-12-16 15:01:15 -0600

    Inicial

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function Index() {
+        return view('auth/register');
+    }
+    public function store(Request $request){
+        $this->validate($request,[
+            'name'=>'required|max:30',
+            'username' => 'required|unique:users|min:3|max:20',
+            'email' => 'required|unique:users|email|max:60',
+            'password' => 'required'
+        ]);
+    }
+}
