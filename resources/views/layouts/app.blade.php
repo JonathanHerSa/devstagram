94c63cca4385fc6d201e94d4eb233b28aef25741
Jonathan Hernandez <jonathan_fenix_95@hotmail.com>
2022-12-19 23:33:29 -0600

    iop

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,10 +12,17 @@
         <header class="p-5 border-b bg-white shadow">
             <div class="container mx-auto flex justify-between items-center">
                 <h1 class="text-3xl font-black">DevStagram</h1>
-                <nav class="flex gap-2 items-center">
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="{{route('login')}}">Log in</a>
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="{{route('register')}}">Registrar</a>
-                </nav>
+                @auth
+                    <nav class="flex gap-2 items-center">
+                        <a class="font-bold uppercase text-gray-600 text-sm" href="{{route('login')}}">Log out</a>
+                    </nav>
+                @endauth
+                @guest
+                    <nav class="flex gap-2 items-center">
+                        <a class="font-bold uppercase text-gray-600 text-sm" href="{{route('login')}}">Log in</a>
+                        <a class="font-bold uppercase text-gray-600 text-sm" href="{{route('register')}}">Registrar</a>
+                    </nav>
+                @endguest
             </div>
         </header>
         <main class="container mx-auto mt-10">
